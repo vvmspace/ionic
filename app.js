1b4a357b0a3ea048427713be7249aa705c70f61b
vvmsecured <iva9Yatoj>
2018-04-10 18:09:39 +0500

    no origins

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ let io      = require('socket.io').listen(http);
 
 app.use(express.static('public'));
 
-io.set('origins', '*.*');
-
 io.on('connection', (socket) => {
     socket.on('data', (data) => {
         io.emit('data', data);
